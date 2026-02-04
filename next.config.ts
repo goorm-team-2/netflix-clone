19b442721af365b2d0bc2b31ccb45927cbab43ff
Chaewon <codenamewont@gmail.com>
2026-02-04 11:58:20 +0900

    Revert "feat : layout"
    This reverts commit 8e24837d6d77ffd8409a91611c59c688ce302481, reversing
    changes made to bc46c80a7074aa6ea0920cd252d0b933655eb2a1.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-=======
   devIndicators: false,
->>>>>>> bc46c80a7074aa6ea0920cd252d0b933655eb2a1
 };
 
 export default nextConfig;
